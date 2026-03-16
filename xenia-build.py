bb3d3166723581381c9e9f36d2eeb31e61a0b7dc
ElectronicHimate <107193409+ElectronicHimate@users.noreply.github.com>
2026-03-16 13:28:54 +0400

    Update xenia-build.py

--- xenia-build.py
+++ xenia-build.py
@@ -184,10 +184,10 @@ def import_vs_environment():
 
     vsdevcmd_path = os.path.join(install_path, "Common7", "Tools", "VsDevCmd.bat")
     if os.access(vsdevcmd_path, os.X_OK):
-        env_tool_args = [vsdevcmd_path, "-arch=amd64", "-host_arch=amd64", "&&", "set"]
+        env_tool_args = [vsdevcmd_path, "-arch=arm64", "-host_arch=arm64", "&&", "set"]
     else:
         vcvars_path = os.path.join(install_path, "VC", "Auxiliary", "Build", "vcvarsall.bat")
-        env_tool_args = [vcvars_path, "x64", "&&", "set"]
+        env_tool_args = [vcvars_path, "arm64", "&&", "set"]
 
     if not version:
         return None
