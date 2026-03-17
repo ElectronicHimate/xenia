80dd69080437290c4be24ca1b5d43cc130fba648
ElectronicHimate <107193409+ElectronicHimate@users.noreply.github.com>
2026-03-17 11:58:02 +0000

    Revert "[XMP] Fixed error caused by incorrect audio allocation for FFMPEG"
    This reverts commit 4b0007bfe53f8ce4aaac1002daa66b69e0d59fae.

--- src/xenia/apu/audio_media_player.cc
+++ src/xenia/apu/audio_media_player.cc
@@ -33,11 +33,11 @@ DEFINE_int32(xmp_default_volume, 70,
 namespace xe {
 namespace apu {
 
-int32_t InitializeAndOpenAvCodec(std::span<uint8_t> song_data,
+int32_t InitializeAndOpenAvCodec(std::vector<uint8_t>* song_data,
                                  AVFormatContext*& format_context,
                                  AVCodecContext*& av_context) {
   AVIOContext* io_ctx =
-      avio_alloc_context(song_data.data(), (int)song_data.size(), 0, nullptr,
+      avio_alloc_context(song_data->data(), (int)song_data->size(), 0, nullptr,
                          nullptr, nullptr, nullptr);
 
   format_context = avformat_alloc_context();
@@ -220,8 +220,9 @@ X_STATUS AudioMediaPlayer::Play(uint32_t playlist_handle, uint32_t song_handle,
 }
 
 void AudioMediaPlayer::Play() {
-  std::span<uint8_t> song_buffer = LoadSongToMemory();
-  if (song_buffer.empty()) {
+  std::vector<uint8_t>* song_buffer = new std::vector<uint8_t>();
+
+  if (!LoadSongToMemory(song_buffer)) {
     return;
   }
 
@@ -395,9 +396,9 @@ X_STATUS AudioMediaPlayer::Previous() {
   return X_STATUS_SUCCESS;
 }
 
-std::span<uint8_t> AudioMediaPlayer::LoadSongToMemory() {
+bool AudioMediaPlayer::LoadSongToMemory(std::vector<uint8_t>* buffer) {
   if (!active_song_) {
-    return {};
+    return false;
   }
 
   // Find file based on provided path?
@@ -409,26 +410,16 @@ std::span<uint8_t> AudioMediaPlayer::LoadSongToMemory() {
       &vfs_file, &file_action);
 
   if (result) {
-    return {};
+    return false;
   }
 
-  std::span<uint8_t> buffer = {
-      static_cast<uint8_t*>(av_malloc(vfs_file->entry()->size())),
-      vfs_file->entry()->size()};
-
-  if (buffer.empty()) {
-    return {};
-  }
-
+  buffer->resize(vfs_file->entry()->size());
   size_t bytes_read = 0;
-  result = vfs_file->ReadSync(buffer, 0, &bytes_read);
-  if (result != X_ERROR_SUCCESS) {
-    // Read failed. We need to manually release resources from av_malloc.
-    av_freep(buffer.data());
-    return {};
-  }
+  result = vfs_file->ReadSync(
+      std::span<uint8_t>(buffer->data(), vfs_file->entry()->size()), 0,
+      &bytes_read);
 
-  return buffer;
+  return !result;
 }
 
 void AudioMediaPlayer::AddPlaylist(uint32_t handle,
